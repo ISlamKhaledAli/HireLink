219289eae7ae7380c2e943145dd8ecec612a9cde
mostafamohamedsalah25 <mostafamohamedsalah25@gmail.com>
2026-05-02 16:38:27 +0300

    feat: install stripe SDK and implement payment controller

--- backend/config/services.php
+++ backend/config/services.php
@@ -41,4 +41,10 @@ return [
         'redirect'      => env('LINKEDIN_REDIRECT_URI'),
     ],
 
+    'stripe' => [
+        'key' => env('STRIPE_KEY'),
+        'secret' => env('STRIPE_SECRET'),
+        'webhook' => env('STRIPE_WEBHOOK_SECRET'),
+    ],
+
 ];
